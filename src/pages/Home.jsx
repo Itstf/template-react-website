941beb8e562f3133ae384c615a731a89cee14f5e
Itstf <favarelli.thaiza@gmail.com>
2023-02-16 18:08:34 -0300

    page-about

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Header from '../partials/Header';
+import PageIllustration from '../partials/PageIllustration';
+import HeroHome from '../partials/HeroHome';
+import FeaturesBlocks from '../partials/FeaturesBlocks';
+import FeaturesZigZag from '../partials/FeaturesZigzag';
+import Testimonials from '../partials/Testimonials';
+import Banner from '../partials/Banner';
+import Footer from '../partials/Footer';
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      <Header />
+      <main className="grow">
+        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+          <PageIllustration />
+        </div>
+        {/*  Page sections */}
+        <HeroHome />
+        <FeaturesBlocks />
+        <FeaturesZigZag />
+        <Testimonials />
+      </main>
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/partials/Footer.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../images/favicon.png';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="py-12 md:py-16">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+          {/* Top area: Blocks */}
+          <div className="grid md:grid-cols-12 gap-8 lg:gap-20 mb-8 md:mb-12">
+
+            {/* 1st block */}
+            <div className="md:col-span-4 lg:col-span-5">
+              <div className="mb-2">
+                {/* Logo */}
+                  <img src={logo} alt="" className='w-28'/>
+              </div>
+              <div className="text-gray-400">Textão sobre o significado da logo</div>
+            </div>
+
+            {/* 2nd, 3rd and 4th blocks */}
+            <div className="md:col-span-8 lg:col-span-7 grid sm:grid-cols-3 gap-8">
+
+              {/* 2nd block */}
+              <div className="text-sm">
+                <h6 className="text-gray-200 font-medium mb-1">Luiz Menezes</h6>
+                <ul>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">GitHub</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Linkedin</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Instagram</Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="text-sm">
+                <h6 className="text-gray-200 font-medium mb-1">Gustavo Nonato</h6>
+                <ul>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">GitHub</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Linkedin</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Instagram</Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="text-sm">
+                <h6 className="text-gray-200 font-medium mb-1">Thaiza Favarelli</h6>
+                <ul>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">GitHub</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Linkedin</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link to="#" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Instagram</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+
+          {/* Bottom area */}
+          <div className="md:flex md:items-center md:justify-between">
+
+            {/* Social links */}
+            <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
+              <li className="ml-4">
+                <Link to="#" className="flex justify-center items-center text-green-600 bg-gray-800 hover:text-gray-100 hover:bg-green-600 rounded-full transition duration-150 ease-in-out" aria-label="Instagram">
+                  <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="20.145" cy="11.892" r="1" />
+                    <path d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z" />
+                    <path d="M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z" />
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+
+            {/* Copyrights note */}
+            <div className="text-gray-400 text-sm mr-4">&copy; OpenMindconnect, All rights reserved.</div>
+
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
